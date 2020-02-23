0df9977f64709bebdfb6e344f4197e82864b29fe
Akim Gubanov <akim.gubanov@gmail.com>
2020-02-23 21:35:30 +0300

    Add Dto and custom Requests for Warehouse

---
+++ src/Dto/Request/Warehouse/WarehouseAddRequest.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Dto\Request\Warehouse;
+
+use JMS\Serializer\Annotation as Serializer;
+
+/**
+ * @package App\Dto\Request\Warehouse
+ */
+class WarehouseAddRequest
+{
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     * @Serializer\SerializedName("code")
+     */
+    private $code;
+
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     * @Serializer\SerializedName("name")
+     */
+    private $name;
+
+    /**
+     * @return string
+     */
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     *
+     * @return self
+     */
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return self
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
---
+++ src/Dto/Warehouse/WarehouseDto.php
@@ -0,0 +1,124 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Dto\Warehouse;
+
+use JMS\Serializer\Annotation as Serializer;
+
+/**
+ * @package App\Dto\Warehouse
+ */
+class WarehouseDto
+{
+    /**
+     * @var int
+     *
+     * @Serializer\Type("int")
+     * @Serializer\SerializedName("id")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     * @Serializer\SerializedName("code")
+     */
+    private $code;
+
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     * @Serializer\SerializedName("name")
+     */
+    private $name;
+
+    /**
+     * @var boolean
+     *
+     * @Serializer\Type("bool")
+     * @Serializer\SerializedName("active")
+     */
+    private $active;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return self
+     */
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     *
+     * @return self
+     */
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return self
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+
+    /**
+     * @param bool $active
+     *
+     * @return self
+     */
+    public function setActive(bool $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+}
